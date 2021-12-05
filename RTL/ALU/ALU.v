1f34fa75ef0e3bff11e639a825a01721bcacc1cb
hohaicongthuan <hohaicongthuan@gmail.com>
2021-12-05 12:41:24 +0700

    Update ALU

--- RTL/ALU/ALU.v
+++ RTL/ALU/ALU.v
@@ -1,7 +1,7 @@
-module ALU(in_rs1, in_rs2, in_ALU_Op, in_fmt, in_sub_aShift_ctrl, out_data, out_ALU_flag);
+module ALU(in_rs1, in_rs2, in_ALU_Op, in_fmt, in_aShift_ctrl, out_data, out_ALU_flag);
     parameter DATA_WIDTH = 64;
 
-    input   in_sub_aShift_ctrl;
+    input   in_aShift_ctrl;
     input   [1:0] in_fmt;
     input   [DATA_WIDTH - 1:0] in_rs1, in_rs2;
     input   [3:0] in_ALU_Op;
@@ -9,13 +9,14 @@ module ALU(in_rs1, in_rs2, in_ALU_Op, in_fmt, in_sub_aShift_ctrl, out_data, out_
     output  [DATA_WIDTH - 1:0] out_data;
     output  [4:0] out_ALU_flag;
 
-    wire    [DATA_WIDTH - 1:0] AddSub_Out, And_Out, Or_Out, Xor_Out, Signed_Cmp_Out, Unsigned_Cmp_Out, Left_Shift_Out, Right_Shift_Out, IntToFP_Out, wire_1, wire_2, wire_3, wire_4, wire_5, wire_6, wire_7, wire_8;
+    wire    [31:0] IntToFP_Out;
+    wire    [DATA_WIDTH - 1:0] AddSub_Out_Sum, AddSub_Out_Diff, And_Out, Or_Out, Xor_Out, Signed_Cmp_Out, Unsigned_Cmp_Out, Left_Shift_Out, Right_Shift_Out, wire_1, wire_2, wire_3, wire_4, wire_5, wire_6, wire_7, wire_8, wire_9, wire_10;
 
     AddSub AddSub_Inst0(
         .in_numA(in_rs1), 
         .in_numB(in_rs2),
-        .in_ctrl_AddSub(in_sub_aShift_ctrl),
-        .out_data(AddSub_Out)
+        .out_Sum(AddSub_Out_Sum),
+        .out_Diff(AddSub_Out_Diff)
     );
     And_Or_Xor And_Or_Xor_Inst0(
         .in_numA(in_rs1),
@@ -34,7 +35,7 @@ module ALU(in_rs1, in_rs2, in_ALU_Op, in_fmt, in_sub_aShift_ctrl, out_data, out_
     Shift Shift_Inst0(
         .in_numA(in_rs1),
         .in_numB(in_rs2),
-        .in_ctrl(in_sub_aShift_ctrl),
+        .in_ctrl(in_aShift_ctrl),
         .out_LeftShift(Left_Shift_Out),
         .out_RightShift(Right_Shift_Out)
     );
@@ -44,7 +45,7 @@ module ALU(in_rs1, in_rs2, in_ALU_Op, in_fmt, in_sub_aShift_ctrl, out_data, out_
         .out_data(IntToFP_Out)
     );
 
-    assign out_data = (in_ALU_Op == 4'b0000) ? AddSub_Out : wire_1;
+    assign out_data = (in_ALU_Op == 4'b0000) ? AddSub_Out_Sum : wire_1;
     assign wire_1 = (in_ALU_Op == 4'b0001) ? And_Out : wire_2;
     assign wire_2 = (in_ALU_Op == 4'b0010) ? Or_Out : wire_3;
     assign wire_3 = (in_ALU_Op == 4'b0011) ? Xor_Out : wire_4;
@@ -52,5 +53,7 @@ module ALU(in_rs1, in_rs2, in_ALU_Op, in_fmt, in_sub_aShift_ctrl, out_data, out_
     assign wire_5 = (in_ALU_Op == 4'b0101) ? Unsigned_Cmp_Out : wire_6;
     assign wire_6 = (in_ALU_Op == 4'b0110) ? Left_Shift_Out : wire_7;
     assign wire_7 = (in_ALU_Op == 4'b0111) ? Right_Shift_Out : wire_8;
-    assign wire_8 = (in_ALU_Op == 4'b1000) ? IntToFP_Out : 64'd0;
+    assign wire_8 = (in_ALU_Op == 4'b1000) ? in_rs2 : wire_9;
+    assign wire_9 = (in_ALU_Op == 4'b1001) ? {32'd0, IntToFP_Out} : wire_10;
+    assign wire_10 = (in_ALU_Op == 4'b1010) ? AddSub_Out_Diff : 64'd0;
 endmodule
--- RTL/ALU/AddSub.v
+++ RTL/ALU/AddSub.v
@@ -1,11 +1,11 @@
 // Integer adder/subtractor module
-module AddSub(in_numA, in_numB, in_ctrl_AddSub, out_data);
+module AddSub(in_numA, in_numB, out_Sum, out_Diff);
     parameter DATA_WIDTH = 64;
 
-    input   in_ctrl_AddSub;
     input   [DATA_WIDTH - 1:0] in_numA, in_numB;
 
-    output  [DATA_WIDTH - 1:0] out_data;
+    output  [DATA_WIDTH - 1:0] out_Sum, out_Diff;
 
-    assign out_data = (!in_ctrl_AddSub) ? (in_numA + in_numB) : (in_numA - in_numB);
+    assign out_Sum = in_numA + in_numB;
+    assign out_Diff = in_numA - in_numB;
 endmodule
